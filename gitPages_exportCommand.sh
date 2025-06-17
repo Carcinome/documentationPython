dafb8ec81c1417eb59c39a857ea60e357c94b3c1
Clement Aicardi <aicardic@home.arpa>
2025-06-17 11:15:42 +0200

    Ajout du script d'automatisation de mise en ligne

---
+++ gitPages_exportCommand.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+make html
+ghp-import -n -p build/html
--- source/conf.py
+++ source/conf.py
@@ -26,5 +26,10 @@ language = 'fr'
 
 html_theme = "sphinx_rtd_theme"
 html_static_path = ['_static']
+html_theme_options = {
+    "collapse_navigation": False,
+    "navigation_depth": -1,
+}
+
 
 html_baseurl = "https://carcinome.github.io/documentationPython/"
